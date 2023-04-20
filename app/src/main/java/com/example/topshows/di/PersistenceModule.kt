c6c789144e3246982c7122c61fca3cba6c4e99ac
Patrik Ángyán <patesz1999@gmail.com>
2023-04-20 14:13:00 +0200

    fixed build errors

--- app/src/main/java/com/example/topshows/di/PersistenceModule.kt
+++ app/src/main/java/com/example/topshows/di/PersistenceModule.kt
@@ -1,10 +1,34 @@
 package com.example.topshows.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.topshows.persistence.AppDatabase
+import com.example.topshows.persistence.TopShowDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object PersistenceModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application): AppDatabase {
+        return Room
+            .databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                "top-shows-db"
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTopShowDao(appDatabase: AppDatabase): TopShowDao {
+        return appDatabase.topShowDao()
+    }
 }
--- app/src/main/java/com/example/topshows/ui/main/MainRepository.kt
+++ app/src/main/java/com/example/topshows/ui/main/MainRepository.kt
@@ -1,11 +1,7 @@
 package com.example.topshows.ui.main
 
-import com.example.topshows.network.ShowService
-import com.example.topshows.persistence.TopShowDao
-import javax.inject.Inject
-
-class MainRepository @Inject constructor(
+class MainRepository /*@Inject constructor(
     private val showService: ShowService,
     private val topShowDao: TopShowDao
-) {
+) */ {
 }

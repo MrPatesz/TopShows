777674bc05f50b1b97315367dc9130b64eb8c093
Patrik Ángyán <patesz1999@gmail.com>
2023-05-20 16:14:16 +0200

    Dao unit tests

--- app/src/main/java/com/example/topshows/model/TopShow.kt
+++ app/src/main/java/com/example/topshows/model/TopShow.kt
@@ -11,4 +11,24 @@ data class TopShow(
     val vote_average: Float,
     val vote_count: Int,
     val poster_path: String?,
-)
+) {
+    companion object {
+        fun mock1(name: String = "True Detective") = TopShow(
+            id = 46648,
+            name = name,
+            first_air_date = "2014-01-12",
+            vote_average = 8.3f,
+            vote_count = 2626,
+            poster_path = "/aowr4xpLP5sRCL50TkuADomJ98T.jpg",
+        )
+
+        fun mock2() = TopShow(
+            id = 40075,
+            name = "Gravity Falls",
+            first_air_date = "2012-06-15",
+            vote_average = 8.5f,
+            vote_count = 2359,
+            poster_path = "/t9inzSLIttATX6RdnmDjL7T4WN7.jpg",
+        )
+    }
+}
